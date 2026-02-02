253fe34f8044477ccffa26ea1ec983e64b30b434
Collin Makwala <collinmakwala@gmail.com>
2026-02-02 18:10:45 +0200

    intiall commit, set-up

---
+++ MiniStore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MiniStore.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
