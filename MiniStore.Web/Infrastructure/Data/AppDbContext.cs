edc233b3c59477aee135809f57039bd71e84104e
Collin Makwala <collinmakwala@gmail.com>
2026-02-02 20:41:55 +0200

    Register DbContext in DI, create migrations + apply DB

--- MiniStore.Web/Infrastructure/Data/AppDbContext.cs
+++ MiniStore.Web/Infrastructure/Data/AppDbContext.cs
@@ -6,4 +6,19 @@ namespace MiniStore.Web.Infrastructure.Data;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Order> Orders => Set<Order>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Order>().OwnsMany(o => o.Items, b =>
+        {
+            b.WithOwner().HasForeignKey("OrderId");
+            b.Property<int>("Id");
+            b.HasKey("Id");
+        });
+    }
 }
